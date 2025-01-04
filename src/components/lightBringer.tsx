d8441482d54a14432a03a7973df1b808b0953fd8
Oliver Larsen <Oliver1703dk@hotmail.com>
2025-01-04 14:31:22 +0100

    Forging the shit out of swords

--- src/components/lightBringer.tsx
+++ src/components/lightBringer.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { EmptyStatement } from 'typescript';
+import { nightTheme } from '../constants/colors';
 
-enum Material {
+export enum Material {
   Fire = "Fire",
   ValyrianSteel = "ValyrianSteel",
   DragonGlass = "DragonGlass",
 }
 
-enum SwordType {
+export enum SwordType {
   Greatsword = "Greatsword",
   Longsword = "Longsword",
   Shortsword = "Shortsword",
@@ -19,11 +20,25 @@ type LightbringerProps = {
   swordType: SwordType;
 };
 
+const swordImages: { [key in SwordType]: any } = {
+  [SwordType.Greatsword]: require("../../assets/images/swordIcons/greatsword.png"),
+  [SwordType.Longsword]: require("../../assets/images/swordIcons/longsword.png"),
+  [SwordType.Shortsword]: require("../../assets/images/swordIcons/shortsword.png"),
+};
 
-export const Lightbringer: React.FC<EmptyStatement> = ({}) => {
+
+export const Lightbringer: React.FC<LightbringerProps> = ({material, swordType}) => {
  
+  const imageSource = swordImages[swordType];
   return (
-    <View style={[styles.container]}>
+    <View style={styles.container}>
+      {imageSource ? (
+        <Image source={imageSource} style={styles.icon} />
+      ) : (
+        <Text style={styles.placeholderText}>No sword type selected</Text>
+      )}
+      <Text style={styles.text}>Material: {material}</Text>
+      <Text style={styles.text}>Sword Type: {swordType}</Text>
     </View>
   );
 };
@@ -38,7 +53,22 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     resizeMode: 'contain',
+    marginBottom: 10, 
   },
+
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 25,
+    marginBottom: 10,
+    backgroundColor: nightTheme.background,
+  },
+
+  placeholderText: {
+    fontSize: 16,
+    color: "gray",
+  },
+  
 });
 
 export default Lightbringer;
--- src/screens/forgeScreen.tsx
+++ src/screens/forgeScreen.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ImageBackground, Button } from "react-native";
 import GOTHeader from "../components/GOTComponents/GOTHeader";
-import Lightbringer from "../components/lightBringer";
+import Lightbringer, { Material, SwordType } from "../components/lightBringer";
+import { nightTheme } from "../constants/colors";
 
 const ForgeScreen: React.FC = () => {
+  const [material, setMaterial] = useState<Material>(Material.Fire);
+  const [swordType, setSwordType] = useState<SwordType>(SwordType.Greatsword);
+
+
+  const handleForge = () => {
+    console.log(`Forged a ${material} ${swordType}!`);
+  };
+  
+
   return (
     <View style={styles.container}>
       <GOTHeader />
@@ -12,7 +22,36 @@ const ForgeScreen: React.FC = () => {
         style={styles.background}
       >
         <View style={styles.content}>
-          <Lightbringer/>
+          <Text style={styles.title}>Select Material:</Text>
+          <View style={styles.buttonRow}>
+            <View style={styles.button}>
+            <Button title="Fire" onPress={() => setMaterial(Material.Fire)} />
+            </View>
+            <View style={styles.button}>
+            <Button title="Valyrian Steel" onPress={() => setMaterial(Material.ValyrianSteel)} />
+            </View>
+            <View style={styles.button}>
+            <Button title="Dragon Glass" onPress={() => setMaterial(Material.DragonGlass)} />
+            </View>
+          </View>
+
+          <Text style={styles.title}>Select Sword Type:</Text>
+          <View style={styles.buttonRow}>
+            <View style={styles.button}>
+            <Button title="Greatsword" onPress={() => setSwordType(SwordType.Greatsword)} />
+            </View>
+            <View style={styles.button}>
+            <Button title="Longsword" onPress={() => setSwordType(SwordType.Longsword)} />
+            </View>
+            <View style={styles.button}>
+            <Button title="Shortsword" onPress={() => setSwordType(SwordType.Shortsword)} />
+            </View>
+          </View>
+
+          <Lightbringer material={material} swordType={swordType} />
+          <View style={styles.button}>
+          <Button title="Forge" onPress={() => handleForge()} />
+          </View> 
         </View>
       </ImageBackground>
     </View>
@@ -33,6 +72,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 10,
+    backgroundColor: nightTheme.background,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: nightTheme.background,
+    paddingVertical: 3,
+    paddingHorizontal: 5,
+    borderRadius: 8,
+    marginHorizontal: 5,
   },
   
 });
